d592331cbb4ae3b5aa0a00f62c5d0e1421b77ace
CelestialDodo <120543993+CelestialDodo@users.noreply.github.com>
2024-07-25 21:09:48 -0700

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -2,13 +2,14 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $dob = htmlspecialchars($_POST['dob']);
+    $email = htmlspecialchars($_POST['email']);
     $reason = htmlspecialchars($_POST['reason']);
 
     $to = 'grammar12131@gmail.com';
     $subject = "License - $name";
-    $message = "Name: $name\nDate of Birth: $dob\n\nReason for Applying:\n$reason";
+    $message = "Name: $name\nDate of Birth: $dob\nEmail: $email\n\nReason for Applying:\n$reason";
     $headers = 'From: no-reply@yourdomain.com' . "\r\n" .
-               'Reply-To: no-reply@yourdomain.com' . "\r\n" .
+               'Reply-To: ' . $email . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
 
     if (mail($to, $subject, $message, $headers)) {
